e52f94855a7d61143ddc444797ae8219571983fb
Franco Sanllehi <francosanllehi@gmail.com>
2023-12-12 17:54:18 -0300

    Basado

--- raspberry/go.mod
+++ raspberry/go.mod
@@ -3,6 +3,7 @@ module klog.co/plantz
 go 1.21.4
 
 require (
+	github.com/a-h/templ v0.2.476 // indirect
 	github.com/labstack/echo/v4 v4.11.3 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- raspberry/main/main.go
+++ raspberry/main/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-  "log"
-  "klog.co/plantz/webServer"
+	"net/http"
+	"github.com/a-h/templ"
+	"klog.co/plantz/comReader"
+	"fmt"
 )
 
 func main() {
-  log.Println("Starting web server")
-  webServer.Start()
+	dataChannel := make(chan comReader.SensorData)
+
+		// // Start the Read function as a goroutine
+	go comReader.Read(dataChannel)
+		
+	http.Handle("/", templ.Handler(hello("franquito")))
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.WriteHeader(http.StatusOK)
+
+		for {
+			// Receive SensorData from the channel
+			sensorData := <-dataChannel
+			
+			fmt.Fprintf(w, "event: humidityUpdate\ndata: %s\n\n", fmt.Sprintf("%s", sensorData.Humidity))
+			fmt.Fprintf(w, "event: temperatureUpdate\ndata: %s\n\n", fmt.Sprintf("%s", sensorData.Temperature))
+			fmt.Fprintf(w, "event: lightUpdate\ndata: %s\n\n", fmt.Sprintf("%s", sensorData.Light))
+			w.(http.Flusher).Flush()
+		}
+
+	})
+	http.ListenAndServe(":8080", nil)
 }
